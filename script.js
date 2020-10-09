4ad9c04483d32524ec2d2aa803faaa4679669b67
Maria Elisa Gomes <mariaelisagmt@gmail.com>
2020-10-08 21:14:50 -0300

    Inserçao function criarCobrinha()

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ let canvas = document.getElementById("snake");
 //Renderiza o desenho dentro do canvas. 
 let context = canvas.getContext("2d");
 let box = 32;
+//Cobrinha
+let snake = [];
+//Estrutura cobrinha
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 function criarBG() {
     //Cor do fundo
@@ -11,4 +18,14 @@ function criarBG() {
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+//Estrutura da cobrinha será um array, no qual será inserido o primeiro e retirado o ultimo
+function criarCobrinha(){
+    for(i = 0; i < snake.length; i++) {
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+
 criarBG();
+criarCobrinha();
