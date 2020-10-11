a5759c88d7b466e5e7e06d5d9162a638ec860c14
Maria Elisa Gomes <mariaelisagmt@gmail.com>
2020-10-11 16:35:45 -0300

    Inserindo condição de parada no jogo

--- script.js
+++ script.js
@@ -52,13 +52,22 @@ function update(event) {
 
 //Main
 function iniciarJogo() {
+
     //Condições para ultrapassar parede
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    //Condição de parada
+    for (i = 1; i <snake.length; i++) {
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
 
+    //Chamada de funções 
     criarBG();
     criarCobrinha();
     drawFood();
